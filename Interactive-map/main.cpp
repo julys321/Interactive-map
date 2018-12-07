97432e024dfe2b14eb446922080d0848ce4935de
Julius Lungys <juliuslungys@gmail.com>
2018-12-07 20:26:11 +0200

    add user interface

--- Interactive-map/main.cpp
+++ Interactive-map/main.cpp
@@ -5,18 +5,35 @@
 using namespace std;
 interactiveMap::Map zemelapis;
 void executeAdminInterface();
+void drawRoom(Room room);
+Room findRoom(std::string room);
 int main() {
-	zemelapis = MapLoader::loadMapFromFile("SavedMap");
+	zemelapis = MapLoader::loadMapFromFile("SavedMap.txt");
 	string userInput;
+	cout << "Kokiame pastate esate?\n";
+	cin >> userInput;
+	string pastatoId = userInput;//asfas
 	while (!userInput._Equal("exit")) {
-		cout << "Kur noretumete nuvykti?\n";
+		cout << "Iveskite kambario numeri kuriame dabar esate:\n";
 		cin >> userInput;
 		if (userInput._Equal("admin")) {
 			userInput = "";
 			executeAdminInterface();
 		}
+		else {
+			Room room = findRoom(userInput);
+			if (room.id == userInput) {
+				drawRoom(room);
+				cout << "Iveskite kambario numeri i kuri noretumete patekti:\n";
+				cin >> userInput;
+			}
+			else {
+				cout << "Neteisingai ivestas kambario numeris\n";
+			}
+			
+		}
 	}
-	MapSaver::writeMapToFile("SavedMap", zemelapis);
+	MapSaver::writeMapToFile("SavedMap.txt", zemelapis);
 	return 0;
 }
 void executeAdminInterface() {
@@ -34,4 +51,22 @@ void executeAdminInterface() {
 		cin >> userInput;
 		zemelapis = MapLoader::loadMapFromFile(userInput);
 	}
+}
+void drawRoom(Room room) {
+	cout << "Kaimyniniai kambariai: ";
+	for (auto connection : room.connections) {
+		cout << connection.connectedWithId << ", ";
+	}
+	cout << "\n";
+	cout << "Daigtai esantys cia: ";
+	for (auto item : room.items) {
+		cout << item.name << " nr" << item.id << ", ";
+	}
+	cout << "\n";
+}
+Room findRoom(std::string room) {
+	for (Room i : zemelapis.rooms)
+		if (i.id._Equal(room))
+			return i;
+	return NULL;
 }
